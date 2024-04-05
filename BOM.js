03babc5643264293f6192898ebd6364a79cc14e7
David Castro <dabid.banegas@gmail.com>
2024-04-05 10:36:56 -0500

    Segundo commit en la branch objectWindow - BOM.js

--- BOM.js
+++ BOM.js
@@ -1,1 +1,7 @@
-alert('BOM || Browser - Object - Model');
+let nombre = prompt('Escriba su nombre');
+if (confirm(nombre + ' Desea eliminar la información?')) {
+    alert('Cuidado ' + nombre + ' perderá todos los datos');
+    
+} else {
+    alert(nombre + ' No tiene permitido eliminar la informacióm');
+}
