1716fe98bd0f744b07a8381099c2047e349c3353
David Castro <dabid.banegas@gmail.com>
2024-04-08 15:30:20 -0500

    commit fin BOOm ventana emerente

---
+++ BOMScreen.js
@@ -0,0 +1,3 @@
+document.write(`Ancho: ${window.screen.pixelDepth} pixeles`)
+console.log(`${window.screen.colorDepth}`);
+alert(`${window.screen.width}`);
---
+++ BOOMLocation.js
@@ -0,0 +1,10 @@
+document.write(`BOMLocation`);
+document.write(`**********`);
+document.write(`${location.href}`);
+document.write(`**********`);
+document.write(`${location.origin}`);
+document.write(`**********`);
+document.write(`${location.protocol}`);
+document.write(`**********`);
+//document.write(`**********`);
+
